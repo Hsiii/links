c22a16fc8421e0998d33e6abbcc55a8ab76b6751
Hsi <its.hsichen@gmail.com>
2026-07-04 12:33:43 +0800

    feat: use svgl link icons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,4 @@
 import type { JSX } from 'react';
-import {
-    AtSign,
-    BriefcaseBusiness,
-    Camera,
-    Code2,
-    ExternalLink,
-    Gamepad2,
-    ImageIcon,
-    Joystick,
-    MessageCircle,
-    MessagesSquare,
-    Music2,
-    UsersRound,
-} from 'lucide-react';
-import type { LucideIcon } from 'lucide-react';
 import Image from 'next/image';
 
 type LinkTone =
@@ -30,7 +15,9 @@ interface SocialLink {
     readonly label: string;
     readonly note: string;
     readonly href: string;
-    readonly Icon: LucideIcon;
+    readonly logoAlt: string;
+    readonly logoSrc?: string;
+    readonly mark?: string;
     readonly tone: LinkTone;
 }
 
@@ -39,77 +26,88 @@ const socialLinks: readonly SocialLink[] = [
         label: 'Facebook',
         note: 'people',
         href: 'https://www.facebook.com/profile.php?id=100011113621038',
-        Icon: UsersRound,
+        logoAlt: 'Facebook',
+        logoSrc: '/svgl/facebook.svg',
         tone: 'blue',
     },
     {
         label: 'Instagram',
         note: 'photos',
         href: 'https://www.instagram.com/ccc_hsi/',
-        Icon: Camera,
+        logoAlt: 'Instagram',
+        logoSrc: '/svgl/instagram.svg',
         tone: 'pink',
     },
     {
         label: 'Threads',
         note: 'posts',
         href: 'https://www.threads.com/@ccc_hsi',
-        Icon: MessagesSquare,
+        logoAlt: 'Threads',
+        logoSrc: '/svgl/threads.svg',
         tone: 'ink',
     },
     {
         label: 'Discord',
         note: 'chat',
         href: 'discord://-/users/917446775873343600',
-        Icon: MessageCircle,
+        logoAlt: 'Discord',
+        logoSrc: '/svgl/discord.svg',
         tone: 'violet',
     },
     {
         label: 'Twitter',
         note: 'posts',
         href: 'https://x.com/OrangeSago',
-        Icon: AtSign,
+        logoAlt: 'X',
+        logoSrc: '/svgl/x.svg',
         tone: 'sky',
     },
     {
         label: 'GitHub',
         note: 'code',
         href: 'https://github.com/Hsiii',
-        Icon: Code2,
+        logoAlt: 'GitHub',
+        logoSrc: '/svgl/github.svg',
         tone: 'ink',
     },
     {
         label: 'Pixiv',
         note: 'art',
         href: 'https://www.pixiv.net/users/64764125',
-        Icon: ImageIcon,
+        logoAlt: 'Pixiv',
+        mark: 'P',
         tone: 'rose',
     },
     {
         label: 'TETR.IO',
         note: 'stacking',
         href: 'https://ch.tetr.io/u/sagocream',
-        Icon: Gamepad2,
+        logoAlt: 'TETR.IO',
+        mark: 'T',
         tone: 'green',
     },
     {
         label: 'Spotify',
         note: 'music',
         href: 'https://open.spotify.com/user/31bturepoosptp5xv2vln3nqz7ca',
-        Icon: Music2,
+        logoAlt: 'Spotify',
+        logoSrc: '/svgl/spotify.svg',
         tone: 'mint',
     },
     {
         label: 'LinkedIn',
         note: 'work',
         href: 'https://www.linkedin.com/in/its-hsi-chen/',
-        Icon: BriefcaseBusiness,
+        logoAlt: 'LinkedIn',
+        logoSrc: '/svgl/linkedin.svg',
         tone: 'blue',
     },
     {
         label: 'Steam',
         note: 'games',
         href: 'https://steamcommunity.com/id/sagocream/',
-        Icon: Joystick,
+        logoAlt: 'Steam',
+        logoSrc: '/svgl/steam.svg',
         tone: 'ink',
     },
 ];
@@ -128,7 +126,6 @@ export function App(): JSX.Element {
                         width={88}
                     />
                     <div className='profile__copy'>
-                        <p className='profile__eyebrow'>hsichen.dev</p>
                         <h1 className='profile__title'>Hsi Chen</h1>
                         <p className='profile__description'>
                             Places I post, play, draw, listen, and connect.
@@ -137,33 +134,49 @@ export function App(): JSX.Element {
                 </header>
 
                 <nav aria-label='Social links' className='social-list'>
-                    {socialLinks.map(({ label, note, href, Icon, tone }) => (
-                        <a
-                            className={`social-link social-link--${tone}`}
-                            href={href}
-                            key={label}
-                            rel='noreferrer noopener'
-                            target='_blank'
-                        >
-                            <span aria-hidden className='social-link__icon'>
-                                <Icon size={22} strokeWidth={2.25} />
-                            </span>
-                            <span className='social-link__copy'>
-                                <span className='social-link__label'>
-                                    {label}
+                    {socialLinks.map(
+                        ({
+                            label,
+                            note,
+                            href,
+                            logoAlt,
+                            logoSrc,
+                            mark,
+                            tone,
+                        }) => (
+                            <a
+                                className={`social-link social-link--${tone}`}
+                                href={href}
+                                key={label}
+                                rel='noreferrer noopener'
+                                target='_blank'
+                            >
+                                <span aria-hidden className='social-link__icon'>
+                                    {logoSrc === undefined ? (
+                                        <span className='social-link__mark'>
+                                            {mark ?? ''}
+                                        </span>
+                                    ) : (
+                                        <Image
+                                            alt={logoAlt}
+                                            className='social-link__logo'
+                                            height={24}
+                                            src={logoSrc}
+                                            width={24}
+                                        />
+                                    )}
                                 </span>
-                                <span className='social-link__note'>
-                                    {note}
+                                <span className='social-link__copy'>
+                                    <span className='social-link__label'>
+                                        {label}
+                                    </span>
+                                    <span className='social-link__note'>
+                                        {note}
+                                    </span>
                                 </span>
-                            </span>
-                            <ExternalLink
-                                aria-hidden
-                                className='social-link__arrow'
-                                size={18}
-                                strokeWidth={2}
-                            />
-                        </a>
-                    ))}
+                            </a>
+                        )
+                    )}
                 </nav>
             </div>
         </main>
